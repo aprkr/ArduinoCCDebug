f6d758e02aa4d88d2c494db4a5ae25eae1c869d4
Alex <2703parker@gmail.com>
2023-01-19 16:05:08 -0600

    Read flash to file

---
+++ python/read.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from common import *
+
+arg_parser = argparse.ArgumentParser( description="Read flash from CC254x" )
+arg_parser.add_argument("path_to_output_file")
+arguments = arg_parser.parse_args()
+file = arguments.path_to_input_file
+
+ser = connectArduino()
+dump = readXData(ser, FLASH_START, FLASH_SIZE)
+with open(file, "wb") as binary_file:
+    binary_file.write(dump)

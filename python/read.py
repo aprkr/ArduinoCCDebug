41014ae4a9fca138e148f529d457180f5e7b20db
Alex <2703parker@gmail.com>
2023-01-30 16:45:08 -0600

    Fix read.py args

--- python/read.py
+++ python/read.py
@@ -5,7 +5,7 @@ from common import *
 arg_parser = argparse.ArgumentParser( description="Read flash from CC254x" )
 arg_parser.add_argument("path_to_output_file")
 arguments = arg_parser.parse_args()
-file = arguments.path_to_input_file
+file = arguments.path_to_output_file
 
 ser = connectArduino()
 dump = readXData(ser, FLASH_START, FLASH_SIZE)
